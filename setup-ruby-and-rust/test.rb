f9bbc90d3bd4270da154f9f3ad486621d44edcb0
Ian Ker-Seymer <hello@ianks.com>
2022-12-11 14:42:21 -0500

    Add test for cache keys

--- setup-ruby-and-rust/test.rb
+++ setup-ruby-and-rust/test.rb
@@ -8,10 +8,33 @@ end
 require "maxitest/autorun"
 require "json"
 
-OUTPUTS = JSON.parse(ENV.fetch("SETUP_OUTPUTS"))
+OUTPUTS = begin
+  JSON.parse(ENV.fetch("SETUP_OUTPUTS"))
+rescue KeyError
+  raise if ENV["CI"]
+
+  {
+    "ruby-platform" => RbConfig::CONFIG["arch"],
+    "ruby-prefix" => RbConfig::CONFIG["prefix"],
+    "base-cache-key-level-1" => "v0__test__it",
+  }
+end
 
 describe "setup-ruby-and-ruby" do
   describe "output validation" do
+    OUTPUTS.each do |key, value|
+      next unless key.include?("cache-key")
+
+      it "has a valid value for #{key.inspect}" do
+        assert value !~ /__\s*__/
+        assert !value.start_with?("__")
+        assert !value.end_with?("__")
+        assert value.start_with?("v")
+
+        assert value.split("__").uniq.size == value.split("__").size
+      end
+    end
+
     it "has has a valid ruby-platform" do
       assert_equal RbConfig::CONFIG["arch"], OUTPUTS["ruby-platform"]
     end
